dca9a9af33517cc33ebb7eddf4bc1217172d61ca
mahadev ashok lanke <mahadev ashok lanke@LAPTOP-5DR3TUC8>
2024-07-12 21:38:57 +0530

    adding more tests

--- GitMavenProj01/src/test/java/com/nt/test/AppTest.java
+++ GitMavenProj01/src/test/java/com/nt/test/AppTest.java
@@ -16,5 +16,24 @@ public class AppTest {
 		assertEquals(expected,actual);
 		
 	}
-
+	
+	@Test
+	public void testWithNegetives() {
+		Arithmetic ar =new Arithmetic();
+		int expected =-40;
+		int actual=ar.sum(-20, -20);
+		assertEquals(expected,actual);
+		
+	}
+	
+	
+	
+	@Test
+	public void testWithZeros() {
+		Arithmetic ar =new Arithmetic();
+		int expected =0;
+		int actual=ar.sum(0, 0);
+		assertEquals(expected,actual);
+		
+	}
 }
