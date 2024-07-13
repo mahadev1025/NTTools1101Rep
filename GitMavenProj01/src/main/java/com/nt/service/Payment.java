6a39c10d6af8f681f51de195067f540911aec567
mahadev ashok lanke <mahadev ashok lanke@LAPTOP-5DR3TUC8>
2024-07-13 17:34:57 +0530

    TL-UPI Payment

--- GitMavenProj01/src/main/java/com/nt/service/Payment.java
+++ GitMavenProj01/src/main/java/com/nt/service/Payment.java
@@ -2,6 +2,11 @@ package com.nt.service;
 
 public class Payment {
 	public String doCardPayment() {
-		return "Do Card Payment is done";	}
+		return "Do Card Payment is done";
+		}
+	
+	public String doUPIPayment() {
+		return "UpI Payment is done";
+	}
 
 }
